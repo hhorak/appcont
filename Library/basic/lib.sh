5f154a2f093c0792117b32f4f9c0c889c46b5413
Honza Horak <hhorak@redhat.com>
2020-04-29 08:35:05 +0200

    Getting rid of internal links

--- Library/basic/lib.sh
+++ Library/basic/lib.sh
@@ -41,15 +41,19 @@ that are part of the RHSCL, RHEL or Fedora products.
 
 =cut
 
-# we use this internal registry for testing
-QUAY_REGISTRY_PREFIX=${QUAY_REGISTRY_PREFIX:-registry-proxy.engineering.redhat.com/rh-osbs/}
+# For some environments, different registry can be used
+CONTAINER_REGISTRY_FLAT_NAMESPACE=0
+CONTAINER_REGISTRY_INSECURE=0
+CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-registry.access.redhat.com}
 
 # adds internal registry for podman or docker
-appcont_basic__add_internal_registris() {
-  sed -i \
-      -e "/\[registries.insecure\]/a registries = ['registry-proxy.engineering.redhat.com']" \
-      -e "/\[registries.insecure\]/{n;d}" \
-      /etc/containers/registries.conf
+appcont_basic__add_insecure_registry() {
+  if [ "${CONTAINER_REGISTRY_INSECURE:-0}" -eq 1 ] ; then
+    sed -i \
+        -e "/\[registries.insecure\]/a registries = ['${CONTAINER_REGISTRY%%/*}']" \
+        -e "/\[registries.insecure\]/{n;d}" \
+        /etc/containers/registries.conf
+  fi
 }
 
 # install docker or podman, whatever is the default choice on that platform
@@ -57,44 +61,24 @@ appcont_basic__add_internal_registris() {
 appcont_basic__get_docker() {
   case $(source /etc/os-release ; echo $VERSION_ID) in
     7*)
-      # for docker package
-      if ! grep rhel-7-server-extras-rpms /etc/yum.repos.d/*repo ; then
-        cat >/etc/yum.repos.d/rhel-extras.repo <<'EOF'
-[rhel-7-server-extras-rpms]
-name = Red Hat Enterprise Linux 7 Extras
-mirrorlist = http://git.app.eng.bos.redhat.com/git/RH_Software_Collections.git/plain/Containers/osbs-repos-signed-pkgs/pulp_multiarch/$basearch/extras
-enabled = 0
-gpgcheck = 1
-gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
-EOF
-      fi
-      # images work with this in the Dockerfile, so we should make sure the rhscl repo is available
-      if ! grep rhel-server-rhscl-7-rpms /etc/yum.repos.d/*repo ; then
-        cat >/etc/yum.repos.d/rhscl-pulp.repo <<'EOF'
-[rhel-server-rhscl-7-rpms]
-name = Red Hat Software Collections Pulp
-mirrorlist = http://git.app.eng.bos.redhat.com/git/RH_Software_Collections.git/plain/Containers/osbs-repos-signed-pkgs/pulp_multiarch/$basearch/rhscl
-enabled = 0
-gpgcheck = 1
-gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
-EOF
-      fi
+      # extras needed for docker package
       yum-config-manager --enable rhel-7-server-extras-rpms
-      yum install -y docker http://download.eng.bos.redhat.com/brewroot/vol/rhel-7/packages/golang-github-cpuguy83-go-md2man/1.0.4/4.el7/x86_64/golang-github-cpuguy83-go-md2man-1.0.4-4.el7.x86_64.rpm
+      yum install -y --enablerepo=epel docker golang-github-cpuguy83-go-md2man
 
-      appcont_basic__add_internal_registris
+      appcont_basic__add_insecure_registry
       systemctl restart docker
     ;;
     *)
       # rhel-8 and fedora
-      yum install -y podman podman-docker
-      appcont_basic__add_internal_registris
+      yum install -y podman podman-docker golang-github-cpuguy83-go-md2man
+      appcont_basic__add_insecure_registry
     ;;
   esac
 }
 
+
 # which branch to take the container sources from
-get_branch_name() {
+appcont_basic__get_branch_name() {
   local version_id=$(source /etc/os-release ; echo $VERSION_ID)
   case ${version_id} in
     7*) git branch -a | grep -o rhscl-3\.[0-9]-rhel-7 | sort | tail -n 1 ;;
@@ -105,7 +89,7 @@ get_branch_name() {
 }
 
 # tests need this variable defined
-get_os_name() {
+appcont_basic__get_os_name() {
   case $(source /etc/os-release ; echo $VERSION_ID) in
     7*) OS=rhel7 ;;
     8*) OS=rhel8 ;;
@@ -113,60 +97,53 @@ get_os_name() {
   esac
 }
 
-# clones the image sources
-appcont_basic__get_image_sources() {
-  local container_component=$1
-  local image_sources_dir=$2
-  git clone git://pkgs.devel.redhat.com/containers/${container_component} ${image_sources_dir}
-}
-
 # parses the parent image from a Dockerfile and pulls it
 appcont_basic__get_parent_image() {
   parent_image=$(grep ^FROM Dockerfile | sed -e 's/FROM\s*//')
-  parent_image_quay=${parent_image//\//-}
-  parent_image_full=${QUAY_REGISTRY_PREFIX}${parent_image_quay}
+  if [ "${CONTAINER_REGISTRY_FLAT_NAMESPACE:-0}" -eq 1 ] ; then
+    parent_image_full=${CONTAINER_REGISTRY}${parent_image//\//-}
+  else
+    parent_image_full=${CONTAINER_REGISTRY}${parent_image}
+  fi
   docker pull ${parent_image_full}
   docker tag ${parent_image_full} ${parent_image}
 }
 
 # prepares a Dockerfile into a new file Dockerfile.tempcopy in CWD
-appcont_basic__prepare_dockerfile() {
-  mkdir -p ./temp_repos
+appcont_basic__prepare_dockerfile_rebuild() {
   cat Dockerfile > Dockerfile.tempcopy
-  for repo_file in /etc/yum.repos.d/*repo ; do
-    sed -i -e "/^FROM/ a\
-ADD ./temp_repos/$(basename $repo_file) $repo_file" Dockerfile.tempcopy
-    cat "$repo_file" >> ./temp_repos/"$(basename $repo_file)"
-  done
+  appcont_basic__prepare_repo_dir_for_dockerfile ./temp_repos Dockerfile.tempcopy
 
   echo "Generating help.1 from README.md and storing into /help.1"
   go-md2man -in "README.md" -out "help.1"
   echo "ADD help.1 /help.1" >> Dockerfile.tempcopy
 }
 
-# main function that is supposed to be called to do all the stuff
-# prepares the environment and process the testing itself
-appcont_basic__test_image() {
-  local container_component=$1
-  local container_version=$2
+appcont_basic__prepare_repo_dir_for_dockerfile() {
+  local output_dir=$1
+  local output_dockerfile=$2
 
-  appcont_basic__get_docker
+  mkdir -p "${output_dir}"
 
-  image_sources_dir=$(mktemp -d ./image_sources_XXXXXX)
-  appcont_basic__get_image_sources "${container_component}" "${image_sources_dir}"
+  sed -i -e "/^FROM/ a\
+ENV SKIP_REPOS_DISABLE=true SKIP_REPOS_ENABLE=true" "${output_dockerfile}"
 
-  pushd "${image_sources_dir}"
+  for repo_file in /etc/yum.repos.d/*repo ; do
+    sed -i -e "/^FROM/ a\
+ADD ${output_dir}/$(basename $repo_file) $repo_file" "${output_dockerfile}"
+    cat "$repo_file" >> "${output_dir}"/"$(basename $repo_file)"
+  done
+}
 
-  git checkout $(get_branch_name)
+# prepares a Dockerfile for the updated use case
+appcont_basic__prepare_dockerfile_updated() {
+  local container_name=$1
+  local packages_update=$2
 
-  appcont_basic__get_parent_image
-  appcont_basic__prepare_dockerfile
+  echo "FROM $container_name" > Dockerfile.tempcopy
+  appcont_basic__prepare_repo_dir_for_dockerfile ./temp_repos Dockerfile.tempcopy
 
-  docker build -t testimage -f Dockerfile.tempcopy .
-
-  IMAGE_NAME=testimage VERSION=${container_version} test/run
-
-  popd # from image_sources
+  echo "RUN yum -y update ${packages_update} && yum -y clean all" >> Dockerfile.tempcopy
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# appcont
-Beakerlib library for application containers
+# appcont: Beakerlib library for application containers testing during RPM verification
+
+Few thoughts this library is designed with:
+
+* The library should work for RHSCL, RHEL and Fedora images
+* For RHSCL and RHEL images, some environment variables need to be re-defined by the test case itself
+* We cover the following two use cases:
+  1. Build an image from source and run the tests from the git repo
+  1. Take the last released image, update the package we test and then run the tests from the git repo
+
+
+## Example of the usage
+
+The following is an example how a concrete test case can look like:
+
+```
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm --all
+        rlAssertBinaryOrigin $NODEJS
+
+        rlRun "rlImport appcont/basic"
+
+        rlRun "appcont_basic__get_docker" 0 "Installing and configuring docker or podman"
+        rlRun "appcont_basic__get_os_name"
+
+        rlRun "node_major=$(node --version | sed -e 's/\..*$//' -e 's/^v//')"
+
+        CONTAINER_SOURCES=https://src.fedoraproject.org/container/nodejs.git
+        CONTAINER_NAME="f$(source /etc/os-release ; echo $VERSION_ID)/nodejs"
+        [ -f container-env.sh ] && source container-env.sh
+
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "git clone CONTAINER_SOURCES '${TmpDir}'" 0 "Getting container image sources"
+        rlRun "pushd $TmpDir"
+
+        rlRun "git checkout $(get_branch_name)" 0 "Switch branch"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "appcont_basic__get_parent_image" 0 "Pull parent image"
+        rlRun "appcont_basic__prepare_dockerfile_rebuild" 0 "Prepare the Dockerfile"
+        rlRun "docker build -t testimage -f Dockerfile.tempcopy ." 0 "Build an image from scratch from the prepared Dockerfile"
+
+        rlRun "IMAGE_NAME=testimage VERSION=${node_major} test/run" 0 "Run the container tests"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "appcont_basic__prepare_dockerfile_updated ${CONTAINER_NAME} 'nodejs npm'" 0 "Prepare the Dockerfile"
+        rlRun "docker build -t testimage -f Dockerfile.tempcopy ." 0 "Build an image from the prepared Dockerfile with updating package"
+
+        rlRun "IMAGE_NAME=testimage VERSION=${node_major} test/run" 0 "Run the container tests"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
+```
